a6e4df09946265233f5508dfe8a480c38322cd3e
Alyssa Brown <alyssa.brown@live.com>
2019-02-06 16:37:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_reader :add_student
+  def initialize (roster)
+    
+    @roster=roster 
+  end 
+  def roster
+    @roster={} 
+  end 
+ 
+  def add_student(student, level)
+    @roster[level] 
+    @roster[level] << student
+  end 
+
+    
+  
+    
+  
+  
+end 
