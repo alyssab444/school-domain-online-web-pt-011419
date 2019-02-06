e32bc41e761ee01a84e4558b60a7180200a9569a
Alyssa Brown <alyssa.brown@live.com>
2019-02-06 16:38:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,7 +14,39 @@ class School
   end 
 
     
+  class School
   
+  
+  
+  attr_accessor  :roster
+  attr_reader :name
+
+  def initialize (name)
+    @name=name
+    @roster= {}
+   
+  end
+
+
+  def add_student (kid, grades)
+    roster[grades] ||= []
+    roster[grades]<< kid
+  end
+  
+  def grade (grades)
+    roster[grades]
+  end
+  
+  def sort 
+  nu_hash = {}
+  roster.each do |grades, kid| 
+    nu_hash[grades] = kid.sort 
+  end 
+  nu_hash
+  end
+  
+
+end
     
   
   
